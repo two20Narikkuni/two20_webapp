1924efe265c4ec2bf016ae199abcaab82b8ab100
Fenar Khan <fenarkhan@gmail.com>
2025-01-31 23:28:21 +0530

    jsdklfjsdlf

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -6,6 +6,9 @@ import Footer from "../components/Footer/Footer";
 export const metadata: Metadata = {
   title: "Two '20 - Empower Change with Just ₹20",
   description: "Join Athaani in making a difference. Donate ₹20 on the 2nd of every month to support those in need. Every contribution counts towards a better tomorrow.",
+  icons: {
+    icon: "/favicon.ico", // Path to your favicon in the public directory
+  },
 };
 
 export default function RootLayout({
